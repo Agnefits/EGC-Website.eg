c9c0398da150ef93fe56b34709a372276d8ef09b
Abdallah Salah <abdallah1662005@gmail.com>
2024-05-08 21:26:11 +0300

    Dashboard added (Test)

--- script/script.js
+++ script/script.js
@@ -1,5 +1,7 @@
 const toggle = document.querySelector(".login_password_icon"),
-    input = document.querySelector(".login_password_field");
+    input = document.querySelector(".login_password_field"),
+    loginForm = document.querySelector(".login_form");
+
 toggle.addEventListener("click", () => {
     if (input.type === "password") {
         input.type = "text";
@@ -8,4 +10,19 @@ toggle.addEventListener("click", () => {
         input.type = "password";
         toggle.setAttribute("src", "../img/hide_password.png");
     }
-})
+});
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let privilage = "";
+    const radios = document.querySelectorAll("input[type=radio]");
+    for (let i = 0; i < radios.length; i++) {
+        if (radios[i].checked) {
+            privilage = radios[i].value;
+            break;
+        }
+    }
+
+    window.location.href = "../index.html?privilage=" + privilage;
+});
