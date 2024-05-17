af87e88c37d4de6a305288703ff104dd7c66292b
Abdallah Salah <abdallah1662005@gmail.com>
2024-05-17 21:31:19 +0300

    Some Updates on add quiz and add doctor
    . في صفحةAdd doctor:
    * إزالة الخانات location و field of scientific و qualifications
    * تغيير choose file إلى choose photo
    * إضافة upload CV
    * إضافة show password

--- script/add-doctor-script.js
+++ script/add-doctor-script.js
@@ -1,30 +1,42 @@
-///Filling Datalist
-const dataListscientificFields = document.getElementById("scientific-fields-data");
-const scientificFieldsData = ["Programming", "Network", "C#", "C++", "Web Development", "Security"];
-scientificFieldsData.forEach(function(value) {
-    dataListscientificFields.innerHTML += " <option value=" + value + "></option>"
-})
+ /*
+           ///Filling Datalist
+           const dataListscientificFields = document.getElementById("scientific-fields-data");
+           const scientificFieldsData = ["Programming", "Network", "C#", "C++", "Web Development", "Security"];
+           scientificFieldsData.forEach(function(value) {
+           dataListscientificFields.innerHTML += " <option value=" + value + "></option>"
+           })
+          
+           //Add Scientific Field
+           const txtScientificFields = document.getElementById("scientific-fields");
+           const btnAddScientificField = document.getElementById("add-scientific-field");
+           const divAddedFields = document.getElementById("added-fields");
+           let addedFieldsArr = [];
 
-//Add Scientific Field
-const txtScientificFields = document.getElementById("scientific-fields");
-const btnAddScientificField = document.getElementById("add-scientific-field");
-const divAddedFields = document.getElementById("added-fields");
-let addedFieldsArr = [];
+           btnAddScientificField.addEventListener("click", function() {
+               if (txtScientificFields.value.trim() !== '' && addedFieldsArr.indexOf(txtScientificFields.value.trim()) === -1) {
+                   addedFieldsArr.push(txtScientificFields.value.trim());
+                   divAddedFields.innerHTML +=
+                       "<div class='scientific-field'>" +
+                       txtScientificFields.value.trim() +
+                       "<div class='delete-scientific-field' onclick='deleteScientificField(this)'>x</div></div>";
+                   txtScientificFields.value = "";
+               }
+           });
+           //Delete Scientific Field
+           function deleteScientificField(element) {
+               element.parentElement.remove();
+               addedFieldsArr = addedFieldsArr.filter(function(value) {
+                   return value !== element.parentElement.innerText.slice(0, -1);
+               });
+           }
+      */
 
-btnAddScientificField.addEventListener("click", function() {
-    if (txtScientificFields.value.trim() !== '' && addedFieldsArr.indexOf(txtScientificFields.value.trim()) === -1) {
-        addedFieldsArr.push(txtScientificFields.value.trim());
-        divAddedFields.innerHTML +=
-            "<div class='scientific-field'>" +
-            txtScientificFields.value.trim() +
-            "<div class='delete-scientific-field' onclick='deleteScientificField(this)'>x</div></div>";
-        txtScientificFields.value = "";
-    }
-});
-//Delete Scientific Field
-function deleteScientificField(element) {
-    element.parentElement.remove();
-    addedFieldsArr = addedFieldsArr.filter(function(value) {
-        return value !== element.parentElement.innerText.slice(0, -1);
-    });
-}
+ //Show and Hide Password
+ document.getElementById("showPassword").addEventListener("click", function() {
+     var x = document.getElementById("password");
+     if (x.type === "password") {
+         x.type = "text";
+     } else {
+         x.type = "password";
+     }
+ });
